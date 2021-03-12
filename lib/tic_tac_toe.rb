534a2b320d437437af49686ac5deb77496158206
Ryan Pearce <ryan.pearce@key.me>
2021-03-12 20:59:52 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,3 +1,14 @@
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [1, 4, 7],
+  [0, 3, 6],
+  [0, 4, 8],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+]
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -68,16 +79,7 @@ def turn(board)
 end
 
 
-WIN_COMBINATIONS = [
-  [0, 1, 2],
-  [1, 4, 7],
-  [0, 3, 6],
-  [0, 4, 8],
-  [2, 5, 8],
-  [2, 4, 6],
-  [3, 4, 5],
-  [6, 7, 8],
-]
+
 
 def won?(board)
   WIN_COMBINATIONS.detect do |combo|
